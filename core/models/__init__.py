bd976d7545a2df4944c8373f31e840fd6ed2bf02
Pavel <savchukpa@icloud.com>
2025-07-21 12:29:29 +0300

    create posts model and migration for users table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = [
     "db_helper",
     "DatabaseHelper",
     "User",
+    "Post",
 ]
 
 from .base import Base
 from .product import Product
 from .db_helper import DatabaseHelper, db_helper
 from .user import User
+from .post import Post
