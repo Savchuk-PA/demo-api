ed5c9d1b271991dd492ad37e23454cef83f58cda
Pavel <savchukpa@icloud.com>
2025-07-21 11:23:54 +0300

    no more create all on start app

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import uvicorn
 from typing import Annotated
 from fastapi import FastAPI, Body, Path
 from pydantic import EmailStr, BaseModel
-from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import roter as users_router
 from api_v1 import router as router_v1
@@ -13,8 +12,6 @@ from core.settings import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
