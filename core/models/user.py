7349d3a8ea9d9215b7b6d4ca30e677f99b4b18aa
Pavel <savchukpa@icloud.com>
2025-07-21 18:49:59 +0300

    create: user relation mixin, profile for user, migrations for profile table

--- core/models/user.py
+++ core/models/user.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
 
 from .base import Base
 
@@ -13,3 +14,4 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
