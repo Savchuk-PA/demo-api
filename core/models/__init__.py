222bd6225d337a4e91b151d54e0349eb87b68197
Pavel <savchukpa@icloud.com>
2025-07-21 12:13:40 +0300

    create user model and migration for users table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,8 +3,10 @@ __all__ = [
     "Product",
     "db_helper",
     "DatabaseHelper",
+    "User",
 ]
 
 from .base import Base
 from .product import Product
 from .db_helper import DatabaseHelper, db_helper
+from .user import User
